62ad50a8c0ad641efed1b09b46cbf3edfffc80dd
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-07 21:52:53 +0100

    added data to controller

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreProjectRequest;
 use App\Http\Requests\UpdateProjectRequest;
 use App\Models\Project;
+use App\Models\Technology;
 use App\Models\Type;
 use Illuminate\Http\Request;
 
@@ -27,7 +28,8 @@ class ProjectController extends Controller
     {
         $project = new Project();
         $types = Type::all();
-        return view('admin.projects.create',compact('project','types'));
+        $technologies = Technology::all();
+        return view('admin.projects.create',compact('project','types','technologies'));
     }
 
     /**
